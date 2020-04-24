bb356cbff79a5fbbb7765fb25016896bba37c6e0
Keevvo <kevin.maiani5k@gmail.com>
2020-04-24 18:03:40 +0200

    Added Navigation Drawer with fragments and CalendarView to display reports

--- app/src/main/java/kevinmaiani/lam2020/healthmonitor/UserActivity.java
+++ app/src/main/java/kevinmaiani/lam2020/healthmonitor/UserActivity.java
@@ -1,26 +1,78 @@
 package kevinmaiani.lam2020.healthmonitor;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.Navigation;
 import kevinmaiani.lam2020.healthmonitor.Models.User;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.CalendarView;
 import android.widget.TextView;
 
-public class UserActivity extends AppCompatActivity {
-    private TextView tvUser;
+import com.google.android.material.navigation.NavigationView;
 
+public class UserActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    //    private TextView tvUser;
+    private DrawerLayout drawer;
+    private CalendarView mCalendarView;
     private User user;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
 
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        ActionBarDrawerToggle toogle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toogle);
+        toogle.syncState();
+
+        if (savedInstanceState == null ) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new CalendarFragment()).commit();
+            navigationView.setCheckedItem(R.id.nav_calendar);
+        }
+
         user = (User) getIntent().getSerializableExtra("User");
 
-        tvUser = findViewById(R.id.tvUser);
+//        mCalendarView = findViewById(R.id.calendarView);
+        //tvUser = findViewById(R.id.tvUser);
 
         if (user != null) {
-            tvUser.setText("BENVENUTO "+user.getName() +" "+user.getLastName());
+            //tvUser.setText("BENVENUTO "+user.getName() +" "+user.getLastName());
         }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer((GravityCompat.START));
+        } else {
+            super.onBackPressed();
+        }
+        super.onBackPressed();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case R.id.nav_calendar:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new CalendarFragment()).commit();
+                break;
+            case R.id.nav_report:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new ReportFragment()).commit();
+                break;
+        }
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
